1b471c667044b7c533aea1242993d9aff18af3d4
syzroy <syzroy@gmail.com>
2019-04-09 19:02:11 +0100

    add stuff

---
+++ src/pages/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "gatsby";
+import Image from "../components/image";
+import "../styles/layout.css";
+
+const IndexPage = () => (
+  <section>
+    <Image />
+    <div style={{ paddingTop: "20vh", paddingLeft: "10vw" }}>
+      <h1>Emotion Analysis</h1>
+    </div>
+    <div style={{ paddingLeft: "10vw", paddingTop: "5vh" }}>
+      <Link to="/camera" className="button">
+        Go to Camera
+      </Link>
+      <Link to="/video" className="button">
+        Go to Video
+      </Link>
+    </div>
+  </section>
+);
+
+export default IndexPage;
---
+++ src/pages/video.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import ListTable from "../components/listTable";
+import { Button } from "@material-ui/core";
+import Image from "../components/image";
+
+class VideoPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null
+    };
+  }
+
+  upload = e => {
+    console.log(e);
+  };
+
+  render() {
+    return (
+      <section>
+        <Image />
+        <section
+          style={{
+            paddingTop: "30vh",
+            paddingLeft: "20vw"
+          }}
+        >
+          <Button
+            variant="outlined"
+            component="span"
+            style={{
+              marginBottom: "1%",
+              color: "white",
+              borderColor: "white",
+              border: "solid"
+            }}
+          >
+            Upload
+          </Button>
+        </section>
+
+        <section
+          style={{
+            paddingLeft: "20vw"
+          }}
+        >
+          <ListTable selected={this.state.selected} />
+        </section>
+      </section>
+    );
+  }
+}
+
+export default VideoPage;
